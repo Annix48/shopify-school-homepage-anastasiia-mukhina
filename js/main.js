50ba0c03dbb94bf27d375a66caa5f1528df0f045
Anastasiia Shevchuk <mainanastasiia@gmail.com>
2025-08-28 19:23:56 +0200

    popup

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const popup = document.getElementById("popup");
-const openBtn = document.querySelector("[data-modal-open]");
 const closeButtons = document.querySelectorAll("[data-popup-close]");
 const form = popup.querySelector(".popup__form");
+const isClosed = localStorage.getItem("popupClosed");
 
-openBtn.addEventListener("click", () => {
-  popup.classList.add("active");
-});
+if (!isClosed) {
+  setTimeout(() => {
+    popup.classList.add("active");
+  }, 1000);
+}
 
 closeButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     popup.classList.remove("active");
+    localStorage.setItem("popupClosed", "true");
   });
 });
 
@@ -17,6 +20,5 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Thank you for subscribing!");
   popup.classList.remove("active");
+  localStorage.setItem("popupClosed", "true");
 });
-
-
